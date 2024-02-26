43ebab08655ff4f5cca10cc18b43a53943541a40
yms06034 <yms06034@gmail.com>
2024-02-26 16:40:02 +0900

    ch

---
+++ logic_code.py
@@ -0,0 +1,190 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+from bs4 import BeautifulSoup as BS
+from datetime import datetime
+
+import pandas as pd
+import time
+import sys, os
+import re
+import json, requests
+import math, openpyxl
+
+
+def find_css(css_selector, browser):
+    return browser.find_element(By.CSS_SELECTOR, css_selector)
+def finds_css(css_selector, browser):
+    return browser.find_elements(By.CSS_SELECTOR, css_selector)
+
+def find_xpath(xpath, browser):
+    return browser.find_element(By.XPATH, xpath)
+def finds_xpath(xpath, browser):
+    return browser.find_elements(By.XPATH, xpath)
+
+def find_id(e_id, browser):
+    return browser.find_element(By.ID, e_id)
+
+def find_className(cn, browser):
+    return browser.find_element(By.CLASS_NAME, cn)
+def finds_className(cn , browser):
+    return browser.find_elements(By.CLASS_NAME, cn)
+
+def find_linktext(lt, browser):
+    return browser.find_element(By.LINK_TEXT, lt)
+
+def find_name(name, browser):
+    return browser.find_element(By.NAME, name)
+def finds_name(name, browser):
+    return browser.find_elements(By.NAME, name)
+
+def find_tagName(tag_name, browser):
+    return browser.find_element(By.TAG_NAME, tag_name)
+
+def finds_tagName(tag_name, browser):
+    return browser.find_elements(By.TAG_NAME, tag_name)
+
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+    return os.path.join(base_path, relative_path)
+
+
+def open_browser():
+    options = webdriver.ChromeOptions()
+    options.add_argument('--no--sandbox')
+    options.add_argument('no-sandbox')
+    options.add_argument('--disable-dev-shm-suage')
+    options.add_argument('--window-size=1080,800')
+    options.add_argument('incognito')
+    chrome_service = Service('chromedriver')
+    chrome_service = Service(executable_path='chromedriver.exe')
+    
+    browser = webdriver.Chrome(service=chrome_service, options=options)
+    
+    return browser
+
+def ka_open_url(browser):
+    browser.get(f'https://map.kakao.com/')
+    browser.implicitly_wait(2)
+
+def search_keyword(browser, ka_kw):
+    find_id('search.keyword.query', browser).clear()
+    find_id('search.keyword.query', browser).send_keys(ka_kw)
+    time.sleep(1)
+    find_id('search.keyword.query', browser).send_keys('\n')
+    time.sleep(1)
+
+    # FInd info.search.place.Count
+    try:
+        place_cnt = find_id('info.search.place.cnt', browser).text
+        return place_cnt
+    except:
+        return 0
+    # place_cnt = place_cnt.replace(',', '')
+    # place_cnt = int(place_cnt)
+    # place_cnt = math.ceil(place_cnt/15)
+    # place_cnt = math.ceil(place_cnt/5)
+
+
+def find_id_list_excetion():
+    find_id_list = []
+
+    for _ in range(1, 150):
+        for i in range(1, 6):
+            find_id_list.append(f'info.search.page.no{i}')
+            if i % 5 == 0:
+                find_id_list.append('info.search.page.next')
+                
+    final_find_id_list = [item for item in find_id_list if item != 'info.search.page.no1']
+
+    return final_find_id_list
+
+def final_logic(browser, ka_kw):
+    ka_open_url(browser)
+    time.sleep(1)
+    re = search_keyword(browser, ka_kw)
+    if re != 0:
+        time.sleep(1)
+        final_find_id_list = find_id_list_excetion()
+        time.sleep(1)
+
+        name_list = []
+        category_list = []
+        tel_list = []
+        addr_list = []
+
+        soup = BS(browser.page_source, 'html.parser')
+
+        for shop_name in soup.find_all(class_='link_name'):
+            name_list.append(shop_name.text)
+
+        for shop_category in soup.find_all(class_='subcategory clickable'):
+            category_list.append(shop_category.text)
+
+        for shop_tel in soup.find_all(class_='phone'):
+            tel_list.append(shop_tel.text)    
+
+        for shop_addr in soup.find_all(class_='addr'):
+            addr_list.append(shop_addr.text)
+
+        browser.execute_script("document.getElementById('info.search.place.more').click();")
+
+        count = 1
+        dt = datetime.now().strftime("%Y-%m-%d")
+        folder = f'{dt} DB'
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+
+        try:
+            for idx, text in enumerate(final_find_id_list):
+                time.sleep(1)
+                soup = BS(browser.page_source, 'html.parser')
+
+                for shop_name in soup.find_all(class_='link_name'):
+                    name_list.append(shop_name.text)
+
+                for shop_category in soup.find_all(class_='subcategory clickable'):
+                    category_list.append(shop_category.text)
+
+                for shop_tel in soup.find_all(class_='phone'):
+                    tel_list.append(shop_tel.text)    
+
+                for shop_addr in soup.find_all(class_='addr'):
+                    addr_list.append(shop_addr.text)
+
+                browser.execute_script(f"document.getElementById('{text}').click();")
+
+                count += 1
+                if count == 34:
+                    break
+        except:
+            df = pd.DataFrame({
+                "name": name_list,
+                "category": category_list,
+                "tel": tel_list,
+                "addr": addr_list,
+            })
+
+            df.to_excel(f'{folder}/{ka_kw} 전화번호 DB.xlsx', index=False)
+            return 2
+
+        time.sleep(1)
+        df = pd.DataFrame({
+            "name": name_list,
+            "category": category_list,
+            "tel": tel_list,
+            "addr": addr_list,
+        })
+
+        df.to_excel(f'{folder}/{ka_kw} 전화번호 DB.xlsx', index=False)
+    else:
+        pass
+    
+    return 1
---
+++ mainCode.py
@@ -0,0 +1,179 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from ui import Ui_MainWindow
+from datetime import datetime
+
+import logic_code as api
+import sys
+import os
+import time
+
+def resource_path(relativa_path):
+    base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+    return os.path.join(base_path, relativa_path)
+
+main_ui = Ui_MainWindow()
+NAME = "Business Number Extraction"
+
+class SeleniumThread(QThread):
+    log_signal = pyqtSignal(str)
+    progress_signal = pyqtSignal(int)
+    finished_signal = pyqtSignal() 
+
+    def __init__(self, main_window, keyword_list=None):
+        super().__init__()
+        self.keyword_list = keyword_list
+        self.main_window = main_window
+        self._running = True
+
+    def run(self):
+        if self.keyword_list == []:
+            self.log_signal.emit("키워드를 넣어주세요.")
+
+        if all([self.keyword_list]):
+            total_iterations = len(self.keyword_list)
+
+            try:
+                self.log_signal.emit("프로그램을 시작합니다.")
+                self.browser = api.open_browser()
+
+                for ka_kw in self.keyword_list:
+                    self.log_signal.emit(f"{ka_kw} 키워드로 전화번호 추출을 시작합니다.")
+                    result = api.final_logic(self.browser, ka_kw)
+                    print("result : ", result)
+
+                    if result == 1:
+                        self.log_signal.emit(f"{ka_kw} 키워드로 추출이 완료되었습니다.")
+                        self.log_signal.emit(f"{ka_kw} 키워드로 엑셀 저장이 완료되었습니다.")
+                        self.log_signal.emit(f"----------------------------")
+
+                        progress = 0
+                        progress = 100 / total_iterations
+                        self.progress_signal.emit(progress)
+
+                    elif result == 2:
+                        self.log_signal(f"{ka_kw}에서 오류가 발생하였습니다. \t\n프로그램을 다시 돌려주세요.")
+
+                self.browser.quit()
+            except Exception as e:
+                self.log_signal(f"{ka_kw}에서 오류가 발생하였습니다. \t\n{str(e)}")
+            finally:
+                self.finished_signal.emit()
+                    
+    def stop(self):
+        self._running = False
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        main_ui.setupUi(self)
+        self.setWindowTitle(NAME)
+
+        window_ico = resource_path('favicon.ico')
+        self.setWindowIcon(QIcon(window_ico))
+        self.timer = QTimer(self)
+        self.browser = None
+
+        self.keyword_list = []
+        self.selenium_thread = None
+
+        main_ui.btn_get_hashtags.clicked.connect(self.btn_get_hashtagsClicked)
+        main_ui.btn_clear_hashtags.clicked.connect(self.btn_clear_hashtagsClicked)
+
+        main_ui.btn_add.clicked.connect(self.btn_addClicked) # Txt file Import button
+        main_ui.btn_del.clicked.connect(self.btn_delClicked) # Txt file Delete button
+        main_ui.hashtag.returnPressed.connect(self.addKeyword)
+
+        main_ui.btn_start.clicked.connect(self.start_btn) # Login Start button
+
+        main_ui.textEdit.setReadOnly(True)
+
+    def addKeyword(self):
+        if main_ui.hashtag.text():
+            if main_ui.hashtag.text() in self.keyword_list:
+                main_ui.hashtag.clear()
+                return
+            self.keyword_list.append(main_ui.hashtag.text())
+            main_ui.hashtags.addItem(main_ui.hashtag.text())
+            main_ui.hashtag.clear()
+        return
+
+    def btn_addClicked(self):
+        self.addKeyword()
+        return
+    
+    def btn_delClicked(self):
+        if main_ui.hashtags.currentItem():
+            self.keyword_list.remove(self.keyword_list[main_ui.hashtags.currentRow()])
+            main_ui.hashtags.takeItem(main_ui.hashtags.currentRow())
+        return 
+
+    def btn_get_hashtagsClicked(self):
+        path = QFileDialog.getOpenFileNames(self)
+        for p in path[0]:
+            try:
+                if p == '':
+                    return
+                
+                if not p.lower().endswith('.txt'):
+                    QMessageBox.information(self, NAME, ".txt 확장자만 넣을 수 있습니다. \t\n파일 확장자를 다시 확인해주세요.")
+                
+                file = open(p, 'rt', encoding='UTF8')
+                while True:
+                    line = file.readline()
+                    if not line:
+                        break
+                    elif line == '\n':
+                        continue
+                    elif line.replace('\n', '') in self.keyword_list:
+                        continue
+                    main_ui.hashtags.addItem(line.replace('\n', ''))
+                    self.keyword_list.append(line.replace('\n', ''))
+
+                file.close()
+            except:
+                j = p.split('/')[-1]
+                QMessageBox.information(self, NAME, f'{j}파일 내용이 잘못되었습니다.')
+                return
+        return
+
+    def btn_clear_hashtagsClicked(self):
+        option = QMessageBox.warning(self, NAME, "정말로 초기화 하시겠습니까?", QMessageBox.Yes | QMessageBox.No)
+        if option == QMessageBox.Yes:
+            main_ui.hashtags.clear()
+            self.keyword_list.clear()
+        return
+    
+    def start_btn(self):
+        try:
+            self.selenium_thread = SeleniumThread(self, self.keyword_list)
+            self.selenium_thread.log_signal.connect(self.update_log)
+            self.selenium_thread.progress_signal.connect(self.update_progress)
+            self.selenium_thread.finished_signal.connect(self.thread_finished)
+            self.selenium_thread.start()
+        except Exception as e:
+            print(e)
+
+    def thread_finished(self):  
+        self.selenium_thread = None  
+    
+    def closeEvent(self, event):
+        if self.selenium_thread is not None and self.selenium_thread.isRunning():
+            self.selenium_thread.stop()
+            self.selenium_thread.wait()
+
+    def update_log(self, message):
+        main_ui.textEdit.append(message)
+
+    def update_progress(self, value):
+        main_ui.progressBar.setValue(value)
+
+
+if __name__=="__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+
+    sys.exit(app.exec_())
